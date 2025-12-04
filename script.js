d99f8da1af13928d604ec74c0ee832fb6ebdf58e
AA_2k7 <amina.ahmad2k7@gmail.com>
2025-12-04 23:21:00 +0500

    initial upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const faqQuestions = document.querySelectorAll('.faq-question');
+
+faqQuestions.forEach(button => {
+  button.addEventListener('click', () => {
+    const answer = button.nextElementSibling;
+    answer.style.display = answer.style.display === 'block' ? 'none' : 'block';
+  });
+});
